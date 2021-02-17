759d46b54202c46f497a0ad761e9ee69556ed821
Alex Dine <alexdine1996@gmail.com>
2021-02-16 19:19:20 -0500

    Initial creation

---
+++ SoccerKicker/Assets/scripts/PlayController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayController : MonoBehaviour
+{
+    [SerializeField] Slider Xforce;
+    [SerializeField] Slider Yforce;
+    [SerializeField] Slider Zforce;
+
+    
+
+    private void kick()
+    {
+        RigidBody.AddForce(Xforce, Yforce, Zforce);
+    }
+}
