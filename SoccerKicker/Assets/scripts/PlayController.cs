cd038c2a0f83447ef2972aefef044ce91a572480
Alex Dine <alexdine1996@gmail.com>
2021-02-16 21:22:13 -0500

    Updated to meet Requirements

--- SoccerKicker/Assets/scripts/PlayController.cs
+++ SoccerKicker/Assets/scripts/PlayController.cs
@@ -9,10 +9,24 @@ public class PlayController : MonoBehaviour
     [SerializeField] Slider Yforce;
     [SerializeField] Slider Zforce;
 
-    
-
-    private void kick()
+    private void Update()
     {
-        RigidBody.AddForce(Xforce, Yforce, Zforce);
+        if (Input.GetKeyDown("space"))
+        {
+
+            kick(Xforce.value, Yforce.value, Zforce.value);
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            gameObject.transform.position = new Vector3(0.0f, 11.3f, 7.6f);
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    private void kick(float x, float y, float z)
+    {
+       
+        gameObject.GetComponent<Rigidbody>().AddForce(x, y, z);
     }
 }
