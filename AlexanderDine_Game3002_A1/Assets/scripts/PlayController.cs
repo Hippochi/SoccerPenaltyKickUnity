420c1038fd195f425f0c2a5a58915f46506c98fe
Alex Dine <alexdine1996@gmail.com>
2021-02-16 21:33:47 -0500

    added proper names according to naming convention

---
+++ AlexanderDine_Game3002_A1/Assets/scripts/PlayController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayController : MonoBehaviour
+{
+    //creates changable at runtime variables to be modified with sliders
+    [SerializeField] Slider Xforce;
+    [SerializeField] Slider Yforce;
+    [SerializeField] Slider Zforce;
+
+    private void Update()
+    {
+        //reads keyboard inputs to activate the two controls
+        if (Input.GetKeyDown("space"))
+        {
+
+            kick(Xforce.value, Yforce.value, Zforce.value);
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            //uses original variables for the soccer ball to "restart" the sim
+            gameObject.transform.position = new Vector3(0.0f, 11.3f, 7.6f);
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+
+    private void kick(float x, float y, float z)
+    {
+       //adds the vector created with sliders to the ball's rigidbody and moves it
+        gameObject.GetComponent<Rigidbody>().AddForce(x, y, z);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-SoccerPenaltyKickUnity
+Made in Unity using c#
+
+Physics simulator that hosts a singular ball that can be "kicked" with a vector of your choosing.
+Drag sliders to change the values, centered at 0 for x, z, and left aligned at 0 for y.
+Pressing space allows the ball to be kicked using current values.
+You can reset the ball with R, returning it to its original position to fall back to the field.
+
